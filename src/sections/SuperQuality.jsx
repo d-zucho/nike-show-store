ac03a468a6eeffe9b165a6faaed0fed6b0d2e289
D_Zucho <danny.nico.dev@outlook.com>
2023-08-21 18:47:25 -0700

    finish services section with styling

--- src/sections/SuperQuality.jsx
+++ src/sections/SuperQuality.jsx
@@ -1,18 +1,45 @@
+import { arrowRight, cheveronRight } from '../assets/icons'
+import { shoe8 } from '../assets/images'
+import Button from '../components/Button'
+
 const SuperQuality = () => {
   return (
-    <section id='superQuality' className='text-left flex'>
-      <div className='w-2/3'>
-        <h1 className='text-4xl font-semibold font-palanquin'>
-          We Provide You <span className='text-coral-red'>Super Quality</span>{' '}
-          Shoes
-        </h1>
-        <p>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corrupti,
-          optio recusandae. Aut repudiandae, minus nulla quis mollitia nesciunt?
-          Similique omnis iure ducimus nulla, pariatur unde? Beatae sapiente
-          expedita aperiam quia, hic nulla nam, numquam dolore dicta temporibus
-          facere unde sint aut, fugit impedit recusandae ut.
+    <section
+      id='about-us'
+      className='text-left flex justify-between items-center max-lg:flex-col gap 10 w-full max-container'
+    >
+      <div className='flex flex-1 flex-col'>
+        <h2 className='mt-10 font-palanquin text-4xl capitalize font-bold text-left lg:max-w-lg'>
+          <p>
+            We provide you
+            <br />
+            <span className='text-coral-red inline-block mt-3'>
+              Super Quality
+            </span>{' '}
+            Shoes
+          </p>
+        </h2>
+        <p className='mt-4 lg:max-w-lg info-text'>
+          Ensuring premium comfort and style, our meticulously crafted footwear
+          is design to elevate your experience, providing you with unmatched
+          quality, innovation, and a touch of elegance.
         </p>
+        <p className='mt-6 lg:max-w-lg info-text'>
+          Our dedication to detail and excellence ensures your satisfgaction
+        </p>
+        <div className='mt-11'>
+          <Button label='Shop Now' iconURL={arrowRight} />
+        </div>
+      </div>
+
+      <div className='flex-1 flex justify-center items-center'>
+        <img
+          className='object-contain'
+          src={shoe8}
+          alt='shoe 8'
+          width={570}
+          height={522}
+        />
       </div>
     </section>
   )
